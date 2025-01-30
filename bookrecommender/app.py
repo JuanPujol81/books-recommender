326d4da64b8b40b570057a3651e3588be8bd8798
Juan Raul Pujol Fonseca <raul810324@gmail.com>
2025-01-30 11:24:55 +0000

    Setting up dara base

--- bookrecommender/app.py
+++ bookrecommender/app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request
 from books import books
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    def __repr__(self):
+        return '<Book %r>' % self.title
+
+
 
 
 
